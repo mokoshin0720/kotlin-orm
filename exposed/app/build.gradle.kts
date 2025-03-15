06d5122c77c47ea9627212ba3e8b581952d25699
mokoshin0720 <shinya.prv@gmail.com>
2025-03-16 01:35:41 +0900

    Exposedライブラリの依存関係を追加し、不要なファイルを削除

--- exposed/app/build.gradle.kts
+++ exposed/app/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
     mavenCentral()
 }
 
+val exposedVersion: String by project
 dependencies {
     // Use the Kotlin JUnit 5 integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -29,6 +30,17 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+
+    implementation("com.h2database:h2:2.2.224")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -40,7 +52,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.AppKt"
+    mainClass = "cmd.AppKt"
 }
 
 tasks.named<Test>("test") {
